8faf851d7399524b853be74797b70830c41f74bd
Dmitriy370 <dim370586@yandex.ru>
2024-07-02 19:31:18 +0300

    Create Task_1.sql
    Task_1.sql

---
+++ Part_1/Block_2/Task_1.sql
@@ -0,0 +1,16 @@
+    INSERT INTO users (id, name, middle_name, last_name, job_title, phone)
+    VALUES (1, 'Sergey', 'Sergeevich', 'Sergeev', 'team-lead', 11-11);
+
+    INSERT INTO users (id, name, middle_name, last_name, job_title, phone)
+    VALUES (1, 'Ivan', 'Ivanovich', 'Ivanov', 'manager', 11-12);
+
+    INSERT INTO users (id, name, middle_name, last_name, job_title, phone)
+    VALUES (1, 'Pavel', 'Pavlovich', 'Pavlov', 'manager', 11-13);
+
+    INSERT INTO organizations (name, adress, phone)
+    VALUES ('Top-Academy', 'г. Тула, центральный переулок 18, кластер Октава, 4 этаж', 84872573787);
+
+    INSERT INTO organizations (name, adress, phone)
+    VALUES ('Fractal Form', 'г. Тула, ул. Кирова, д.135, пом.48', 89539717800);
+
+    INSERT INTO clients(id, name, middle_name, last_name)
