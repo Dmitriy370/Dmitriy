0d6ebce30a001b6d66418e10e36e9cda7dd06399
Dmitriy370 <dim370586@yandex.ru>
2024-07-02 20:43:36 +0300

    Update Task_1.sql
    Task_1 update

--- Part_1/Block_2/Task_1.sql
+++ Part_1/Block_2/Task_1.sql
@@ -2,10 +2,10 @@
     VALUES (1, 'Sergey', 'Sergeevich', 'Sergeev', 'team-lead', 11-11);
 
     INSERT INTO users (id, name, middle_name, last_name, job_title, phone)
-    VALUES (1, 'Ivan', 'Ivanovich', 'Ivanov', 'manager', 11-12);
+    VALUES (2, 'Ivan', 'Ivanovich', 'Ivanov', 'manager', 11-12);
 
     INSERT INTO users (id, name, middle_name, last_name, job_title, phone)
-    VALUES (1, 'Pavel', 'Pavlovich', 'Pavlov', 'manager', 11-13);
+    VALUES (3, 'Pavel', 'Pavlovich', 'Pavlov', 'manager', 11-13);
 
     INSERT INTO organizations (name, adress, phone)
     VALUES ('Top-Academy', 'г. Тула, центральный переулок 18, кластер Октава, 4 этаж', 84872573787);
@@ -13,4 +13,10 @@
     INSERT INTO organizations (name, adress, phone)
     VALUES ('Fractal Form', 'г. Тула, ул. Кирова, д.135, пом.48', 89539717800);
 
-    INSERT INTO clients(id, name, middle_name, last_name)
+    INSERT INTO clients (id, name, middle_name, last_name, job_title, phone, organization_name)
+    VALUES (1, 'Sergey', 'Sergeevich', 'Sergeev', 'CEO', 555-555, 'Top-Academy');
+
+    INSERT INTO clients (id, name, middle_name, last_name, job_title, phone, organization_name)
+    VALUES (2, 'Ivan', 'Ivanovich', 'Ivanov', 'manager', 777-777, 'Fractal Form');
+
+    
