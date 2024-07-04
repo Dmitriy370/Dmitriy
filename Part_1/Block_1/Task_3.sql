7826fa47cc498eb5aeceec14e94fb654223ce9d6
Dmitriy370 <dim370586@yandex.ru>
2024-07-04 20:11:48 +0300

    Update Task_3.sql
    Update Task_3 Block_1

--- Part_1/Block_1/Task_3.sql
+++ Part_1/Block_1/Task_3.sql
@@ -1,23 +1,23 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(16) NOT NULL,
-    middle_name VARCHAR(16),
-    last_name   VARCHAR(16) NOT NULL,
+    name TEXT NOT NULL,
+    middle_name TEXT,
+    last_name   TEXT NOT NULL,
     job_title   VARCHAR(16) NOT NULL,
     phone       VARCHAR(16)
     );
 
 CREATE TABLE organizations(
-    name VARCHAR PRIMARY KEY NOT NULL,
+    name VARCHAR(16) PRIMARY KEY NOT NULL,
     adress VARCHAR(1024),
     phone VARCHAR(16)
     );
 
 CREATE TABLE clients(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(16) NOT NULL,
-    middle_name VARCHAR(16),
-    last_name VARCHAR(16) NOT NULL,
+    name TEXT NOT NULL,
+    middle_name TEXT,
+    last_name TEXT NOT NULL,
     job_title VARCHAR(16),
     phone VARCHAR(16),
     FOREIGN KEY (organization_name) REFERENCES organizations (name)
@@ -25,9 +25,9 @@ CREATE TABLE clients(
 
 CREATE TABLE documents(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(16),
+    name TEXT,
     document_date DATE NOT NULL,
-    status VARCHAR NOT NULL,
+    status TEXT NOT NULL,
     FOREIGN KEY (id_user) REFERENCES users (id),
     FOREIGN KEY (id_client) REFERENCES clients(id)
     );
